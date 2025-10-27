d5b70d47a1d8033f98178bdea07b3a477d940019
Roger Mateo <rogermateom@gmail.com>
2025-10-27 17:52:27 +0100

    CRUD operations working

--- backend/src/main/java/com/helpdesk/backend/model/User.java
+++ backend/src/main/java/com/helpdesk/backend/model/User.java
@@ -10,12 +10,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+
+
 
 @Entity
 // Table name is a reserved word in many DBs. In your SQL it's quoted "user".
@@ -26,6 +30,7 @@ import jakarta.persistence.UniqueConstraint;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id_pk", nullable = false)
     private Long id;
 
