8ca63a0dd4d3b70640eb801be940b5d28ba09a74
Roger Mateo <rogermateom@gmail.com>
2025-10-30 19:53:17 +0100

    Basic content added

---
+++ frontend/src/api/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <h2 className="text-xl font-semibold">Hola 👋 — Home privada</h2>
+    </div>
+  );
+}
